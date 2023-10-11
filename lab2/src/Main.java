1ca74eac6279274409da6a79143e8d9592cb28a1
Vlad <durdeuvladioan@gmail.com>
2023-10-11 19:28:31 +0300

    Lab 2 + bonus point

---
+++ lab2/src/Main.java
@@ -0,0 +1,24 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Scrie un numar: ");
+        int citire = scanner.nextInt();
+        System.out.println("Ai scris "+citire);
+        byte[] a = new byte[10];
+        BigInteger bigInteger = new BigInteger(a);
+        for(int i=0;i<citire;i++){
+           bigInteger = bigInteger.add(BigInteger.TWO.pow(i));
+        }
+        System.out.println(bigInteger.toString());
+//        System.out.println(Integer.MAX_VALUE);
+//        System.out.println(Integer.MAX_VALUE+10);
+//        System.out.println(Double.MAX_VALUE + 1);
+//        System.out.println(Double.MAX_VALUE / 0);
+        // incarcam 6.6 pe git saptamana viitoare pt bonus
+    }
+
+
+}
